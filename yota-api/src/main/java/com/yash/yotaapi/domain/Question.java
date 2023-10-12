ca7e679739eb69816ecb2d262358e087c79231ef
Pragati Sanjay Paliwal <pragati.paliwal@yash.com>
2023-10-12 14:27:29 +0530

    question technology module changes are done

--- yota-api/src/main/java/com/yash/yotaapi/domain/Question.java
+++ yota-api/src/main/java/com/yash/yotaapi/domain/Question.java
@@ -1,8 +1,7 @@
 package com.yash.yotaapi.domain;
 
 import java.util.Date;
-
-
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,8 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
-
-
+import javax.persistence.Transient;
 import javax.validation.constraints.NotBlank;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -21,7 +19,6 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 
 /**
  * This Question Management model will work as a data transfer object. Field
@@ -37,20 +34,18 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 
-
 public class Question {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	/**
-	 *  Question type of the Question should not be empty
+	 * Question type of the Question should not be empty
 	 */
-	
+
 	@NotBlank(message = "Question type is required")
 	private String questionType;
-	
 
 	/**
 	 * Question level of the Question should not be empty
@@ -64,43 +59,41 @@ public class Question {
 	@NotBlank(message = "Question is required")
 	private String question;
 
-	
 	/**
 	 * Answer type should not be empty
 	 */
 	@NotBlank(message = "Answer type is required")
 	private String answerType;
-	
+
 	/**
 	 * Option should not be empty
 	 */
 	@NotBlank(message = "Option A is required")
 	private String a;
-	
+
 	/**
 	 * Option should not be empty
 	 */
 	@NotBlank(message = "Option B is required")
 	private String b;
-	
+
 	/**
 	 * Option should not be empty
 	 */
 	@NotBlank(message = "Option C is required")
 	private String c;
-	
+
 	/**
 	 * Option should not be empty
 	 */
 	@NotBlank(message = "Option D is required")
 	private String d;
-	
+
 	/**
 	 * Answer of the Question should not be empty
 	 */
 	@NotBlank(message = "Answer is required")
 	private String correctAnswer;
-
 
 	/**
 	 * createDate of the Question Bank, It will be automatically generated at the
@@ -111,16 +104,15 @@ public class Question {
 	@JsonFormat(pattern = "yyyy-MM-dd")
 	private Date created_At;
 
-
 	/**
 	 * upatedDate of the Question Bank, It will be automatically generated at the
 	 * time of record updation. You don't need to add the updated date manually from
 	 * client end
 	 */
 
-	@JsonFormat(pattern="yyyy-MM-dd")
+	@JsonFormat(pattern = "yyyy-MM-dd")
 	private Date updated_At;
-	
+
 	/**
 	 * This method will be called before the entity is inserted (persisted) into the
 	 * database.
@@ -137,4 +129,10 @@ public class Question {
 	public void onUpdate() {
 		this.updated_At = new Date();
 	}
+	
+	private Long parentTechnology_id;
+	
+	@Transient
+  public List<ParentTechnology> parentTechnology;
+
 }
