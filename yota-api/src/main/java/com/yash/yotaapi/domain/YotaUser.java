82ade13f2659fcd2d44f13774a6a69a52bdf866e
Ram Babu Verma <ram.verma@yash.com>
2023-10-17 13:36:39 +0530

    Code refactored after sync with development branch

--- yota-api/src/main/java/com/yash/yotaapi/domain/YotaUser.java
+++ yota-api/src/main/java/com/yash/yotaapi/domain/YotaUser.java
@@ -10,6 +10,7 @@ import javax.persistence.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "YOTA_USER")
 public class YotaUser {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
