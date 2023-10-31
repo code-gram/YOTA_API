f967ef1ad1d80a644a87b5453271111625a9cc57
Ram Babu Verma <ram.verma@yash.com>
2023-10-31 21:39:22 +0530

    Code Refactored with upstream development branch

--- yota-api/src/main/java/com/yash/yotaapi/controller/TechnologyMasterController.java
+++ yota-api/src/main/java/com/yash/yotaapi/controller/TechnologyMasterController.java
@@ -1,28 +1,18 @@
 package com.yash.yotaapi.controller;
 
-import java.util.List;
-import javax.validation.Valid;
-
-import com.yash.yotaapi.serviceimpl.MasterServiceImpl;
+import com.yash.yotaapi.domain.TechnologyMaster;
+import com.yash.yotaapi.service.TechnologyMasterService;
+import com.yash.yotaapi.util.FieldErrorValidationUtillity;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import com.yash.yotaapi.domain.TechnologyMaster;
-import com.yash.yotaapi.util.FieldErrorValidationUtillity;
+import org.springframework.web.bind.annotation.*;
 
-
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import javax.validation.Valid;
+import java.util.List;
 
 
 /** Parent Technology Controller will facilitates CRUD functionalities
@@ -42,7 +32,7 @@ public class TechnologyMasterController {
 	 * ParentTechnologyService is used to interact controller layer with service layer.
 	 */
 	@Autowired
-	private MasterServiceImpl.TechnologyMasterService technologyMasterService;
+	private TechnologyMasterService technologyMasterService;
 	
 	/**
 	 * FieldErrorValidationService is used to validate field errors.
