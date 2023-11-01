30c631b3f960cb51011c152ed3c7759afb826bbf
Pragati Sanjay Paliwal <pragati.paliwal@yash.com>
2023-11-01 16:00:24 +0530

    technology crud backed up to date code

--- yota-api/src/main/java/com/yash/yotaapi/controller/TechnologyMasterController.java
+++ yota-api/src/main/java/com/yash/yotaapi/controller/TechnologyMasterController.java
@@ -1,12 +1,13 @@
 package com.yash.yotaapi.controller;
 
+import javax.validation.Valid;
 import java.util.List;
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,11 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.yash.yotaapi.domain.TechnologyMaster;
 import com.yash.yotaapi.service.TechnologyMasterService;
 import com.yash.yotaapi.util.FieldErrorValidationUtillity;
-
-
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-
+import com.yash.yotaapi.util.FieldErrorValidationUtillity;
 
 /**
  * Parent Technology Controller will facilitates CRUD functionalities
@@ -32,111 +31,107 @@ import io.swagger.v3.oas.annotations.tags.Tag;
  */
 @CrossOrigin("*")
 
-
 @Tag(name = "Technology Controller", description = "Controller for Technology")
 
 @RestController
 @RequestMapping("/yota/api/technologies")
 public class TechnologyMasterController {
 
-    /**
-     * ParentTechnologyService is used to interact controller layer with service layer.
-     */
-    @Autowired
-    private TechnologyMasterService technologyMasterService;
+	/**
+	 * ParentTechnologyService is used to interact controller layer with service
+	 * layer.
+	 */
+	@Autowired
+	private TechnologyMasterService technologyMasterService;
 
-    /**
-     * FieldErrorValidationService is used to validate field errors.
-     */
-    @Autowired
-    private FieldErrorValidationUtillity validationService;
+	/**
+	 * FieldErrorValidationService is used to validate field errors.
+	 */
+	@Autowired
+	private FieldErrorValidationUtillity validationService;
 
+	/**
+	 * addParentTechnology method is used add ParentTechnology through service layer
+	 *
+	 * @param technology
+	 * @return generic type
+	 */
 
-    /**
-     * addParentTechnology method is used add ParentTechnology through service layer
-     *
-     * @param technology
-     * @return generic type
-     */
+	@PostMapping("/")
+	public ResponseEntity<?> addTechnology(@Valid @RequestBody TechnologyMaster technology, BindingResult result) {
+		ResponseEntity<?> errorMap = validationService.validationError(result);
+		if (errorMap != null) {
+			return errorMap;
+		}
+		return new ResponseEntity<TechnologyMaster>(technologyMasterService.save(technology), HttpStatus.OK);
+	}
 
-    @PostMapping("/")
-    public ResponseEntity<?> addTechnology(@Valid @RequestBody TechnologyMaster technology, BindingResult result) {
-        ResponseEntity<?> errorMap = validationService.validationError(result);
-        if (errorMap != null) {
-            return errorMap;
-        }
-        return new ResponseEntity<TechnologyMaster>(technologyMasterService.save(technology), HttpStatus.OK);
-    }
+	/**
+	 * getAll method is used to fetch all existing parent technology from DB
+	 *
+	 * @return List of ParentTechnology
+	 */
 
+	@GetMapping("/")
+	public ResponseEntity<List<TechnologyMaster>> getTechnologies() {
+		return new ResponseEntity<List<TechnologyMaster>>(technologyMasterService.getAllTechs(), HttpStatus.OK);
+	}
 
-    /**
-     * getAll method is used to fetch all existing parent technology from DB
-     *
-     * @return List of ParentTechnology
-     */
+	/**
+	 * getTech method is used to get ParentTechnology from DB on basis name.
+	 *
+	 * @param name
+	 * @return ParentTechnology
+	 */
 
-    @GetMapping("/")
-    public ResponseEntity<List<TechnologyMaster>> getTechnologies() {
-        return new ResponseEntity<List<TechnologyMaster>>(technologyMasterService.getAllTechs(), HttpStatus.OK);
-    }
+	@GetMapping("/{id}")
+	public ResponseEntity<TechnologyMaster> getTechnology(@PathVariable(value = "id") long id) {
+		return new ResponseEntity<TechnologyMaster>(technologyMasterService.getTech(id), HttpStatus.OK);
+	}
 
+	/**
+	 * searchTech method is used to get ParentTechnology from DB on basis Keyword.
+	 *
+	 * @param keyword
+	 * @return List of ParentTechnology
+	 */
 
-    /**
-     * getTech method is used to get ParentTechnology from DB on basis name.
-     *
-     * @param name
-     * @return ParentTechnology
-     */
+	@GetMapping("/search/{keyword}")
+	public ResponseEntity<List<TechnologyMaster>> searchTechnology(@PathVariable("keyword") String keyword) {
+		List<TechnologyMaster> technologies = technologyMasterService.searchTech(keyword);
+		return new ResponseEntity<List<TechnologyMaster>>(technologies, HttpStatus.OK);
+	}
 
-    @GetMapping("/{name}")
-    public ResponseEntity<TechnologyMaster> getTechnology(@PathVariable(value = "name") String name) {
-        return new ResponseEntity<TechnologyMaster>(technologyMasterService.getTech(name), HttpStatus.OK);
-    }
+	/**
+	 * removeTech method is used to delete ParentTechnology by id.
+	 *
+	 * @param id
+	 * @return Generic type
+	 */
 
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> removeTechnology(@PathVariable(value = "id") long id) {
+		technologyMasterService.removeTech(id);
+		return new ResponseEntity<String>("Technology with ID :" + id + " deleted.", HttpStatus.OK);
+	}
 
-    /**
-     * searchTech method is used to get ParentTechnology from DB on basis Keyword.
-     *
-     * @param keyword
-     * @return List of ParentTechnology
-     */
+	/**
+	 * upadateTech method is used update the existing ParentTechnology from DB
+	 * otherwise it will save one into DB
+	 *
+	 * @param technology
+	 * @param result
+	 * @return Updated ParentTechnology
+	 */
 
-    @GetMapping("/search/{keyword}")
-    public ResponseEntity<List<TechnologyMaster>> searchTechnology(@PathVariable("keyword") String keyword) {
-        List<TechnologyMaster> technologies = technologyMasterService.searchTech(keyword);
-        return new ResponseEntity<List<TechnologyMaster>>(technologies, HttpStatus.OK);
-    }
-
-
-    /**
-     * removeTech method is used to delete ParentTechnology by id.
-     *
-     * @param id
-     * @return Generic type
-     */
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> removeTechnology(@PathVariable(value = "id") long id) {
-        technologyMasterService.removeTech(id);
-        return new ResponseEntity<String>("Technology with ID :" + id + " deleted.", HttpStatus.OK);
-    }
-
-
-    /**
-     * upadateTech method is used update the existing ParentTechnology from DB otherwise it will save one into DB
-     *
-     * @param technology
-     * @param result
-     * @return Updated ParentTechnology
-     */
-
-    @PutMapping("/")
-    @ApiResponse(responseCode = "200", description = "successfully updated")
-    public ResponseEntity<?> updateTechnology(@Valid @RequestBody TechnologyMaster technology, BindingResult result) {
-        ResponseEntity<?> errorMap = validationService.validationError(result);
-        if (errorMap != null) {
-            return errorMap;
-        }
-        return new ResponseEntity<TechnologyMaster>(technologyMasterService.updateTech(technology), HttpStatus.OK);
-    }
+	@PutMapping("/{id}")
+	@ApiResponse(responseCode = "200", description = "successfully updated")
+	public ResponseEntity<?> updateTechnology(@Valid @RequestBody TechnologyMaster technology, BindingResult result,
+			@PathVariable long id) {
+		ResponseEntity<?> errorMap = validationService.validationError(result);
+		if (errorMap != null) {
+			return errorMap;
+		}
+		return new ResponseEntity<TechnologyMaster>(technologyMasterService.updateTech(technology, id), HttpStatus.OK);
+	}
 }
--- yota-api/src/main/java/com/yash/yotaapi/serviceimpl/TechnologyMasterServiceImpl.java
+++ yota-api/src/main/java/com/yash/yotaapi/serviceimpl/TechnologyMasterServiceImpl.java
@@ -9,22 +9,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import com.yash.yotaapi.domain.Client;
 import com.yash.yotaapi.domain.TechnologyMaster;
 import com.yash.yotaapi.exception.ParentTechnologyException;
 import com.yash.yotaapi.exception.ParentTechnologyNotFoundException;
 import com.yash.yotaapi.repository.TechnologyMasterRepository;
 import com.yash.yotaapi.service.TechnologyMasterService;
 
-
-/**This is service layer class for Parent Technology to write business logic
+/**
+ * This is service layer class for Parent Technology to write business logic
+ * 
  * @author pratik.kurbet
  *
  */
 @Service
 public class TechnologyMasterServiceImpl implements TechnologyMasterService {
-	
+
 	/**
-	 * ParentTechnologyRepository is used to interact service layer with repository layer.
+	 * ParentTechnologyRepository is used to interact service layer with repository
+	 * layer.
 	 */
 	@Autowired
 	private TechnologyMasterRepository technologyMasterRepository;
@@ -35,15 +38,16 @@ public class TechnologyMasterServiceImpl implements TechnologyMasterService {
 	@Override
 	public TechnologyMaster save(TechnologyMaster technology) {
 		try {
-			technology.setName(technology.getName().toUpperCase());
+			technology.setId(technology.getId());
 			return technologyMasterRepository.save(technology);
 		} catch (DataIntegrityViolationException e) {
-			throw new ParentTechnologyException("Technology with Name  "+technology.getName().toUpperCase()+" already exists!!");
+			throw new ParentTechnologyException("Technology with Name  " + technology.getId() + " already exists!!");
 		}
 	}
 
 	/**
-	 * This method is for get all ParentTechnologies from DB through repository layer
+	 * This method is for get all ParentTechnologies from DB through repository
+	 * layer
 	 */
 	@Override
 	public List<TechnologyMaster> getAllTechs() {
@@ -59,20 +63,20 @@ public class TechnologyMasterServiceImpl implements TechnologyMasterService {
 		try {
 			technologyMasterRepository.findById(id).get();
 		} catch (NoSuchElementException e) {
-			throw new ParentTechnologyException("Technology with ID :"+id+" does not exist");
+			throw new ParentTechnologyException("Technology with ID :" + id + " does not exist");
 		}
 		technologyMasterRepository.deleteById(id);
-		
+
 	}
-	
+
 	/**
 	 * This method is for update ParentTechnology to DB through repository layer
 	 */
 	@Override
 	@Transactional
 	public TechnologyMaster updateTech(TechnologyMaster technology) {
-		TechnologyMaster existParentTechnology= technologyMasterRepository.getByName(technology.getName());
-		if (existParentTechnology==null) {
+		TechnologyMaster existParentTechnology = technologyMasterRepository.getByName(technology.getName());
+		if (existParentTechnology == null) {
 			return technologyMasterRepository.save(technology);
 		} else {
 			existParentTechnology.setName(technology.getName());
@@ -83,26 +87,45 @@ public class TechnologyMasterServiceImpl implements TechnologyMasterService {
 		return technology;
 	}
 
+	@Override
+	public TechnologyMaster updateTech(TechnologyMaster technology, long id) {
+
+		TechnologyMaster technologyDetails = technologyMasterRepository.findById(id).get();
+		if (technologyDetails == null) {
+			throw new ParentTechnologyNotFoundException(
+					"TechnologyMaster id: " + id + " is not present in TechnologyDetails ");
+		} else {
+			technologyDetails.setName(technology.getName());
+			technologyDetails.setShortDescription(technology.getShortDescription());
+
+			TechnologyMaster updateTech = technologyMasterRepository.save(technologyDetails);
+			return updateTech;
+		}
+	}
+
 	/**
-	 * This method is for get ParentTechnology from DB through repository layer on basis of name only.
+	 * This method is for get ParentTechnology from DB through repository layer on
+	 * basis of name only.
 	 */
 	@Override
-	public TechnologyMaster getTech(String name) {
-		TechnologyMaster technology= technologyMasterRepository.getByName(name.toUpperCase());
-		if (technology==null) {
-			throw new ParentTechnologyNotFoundException("Technology with name : "+name+" does not exist");
+	public TechnologyMaster getTech(long id) {
+		TechnologyMaster technology = technologyMasterRepository.findById(id).get();
+		if (technology == null) {
+			throw new ParentTechnologyNotFoundException("Technology with name : " + id + " does not exist");
 		}
 		return technology;
 	}
-	
+
 	/**
-	 * This method is for get ParentTechnology from DB through repository layer on basis of keyword only.
+	 * This method is for get ParentTechnology from DB through repository layer on
+	 * basis of keyword only.
 	 */
 	@Override
 	public List<TechnologyMaster> searchTech(String keyword) {
-		List<TechnologyMaster> list= technologyMasterRepository.getByNameContaining(keyword.toUpperCase());
+		List<TechnologyMaster> list = technologyMasterRepository.getByNameContaining(keyword.toUpperCase());
 		if (list.isEmpty()) {
-			throw new ParentTechnologyNotFoundException("Technology containing keyword  : "+keyword+" does not exist");
+			throw new ParentTechnologyNotFoundException(
+					"Technology containing keyword  : " + keyword + " does not exist");
 		}
 		return technologyMasterRepository.getByNameContaining(keyword.toUpperCase());
 	}
