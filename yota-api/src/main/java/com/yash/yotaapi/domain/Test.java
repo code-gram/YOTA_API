cd62ce8dbb204bfccc8e36fd791512d6bdb7931f
gaurav30390 <gaurav30390@gmail.com>
2024-02-04 16:21:24 +0530

    added extra fields and relations in Test.java

--- yota-api/src/main/java/com/yash/yotaapi/domain/Test.java
+++ yota-api/src/main/java/com/yash/yotaapi/domain/Test.java
@@ -1,15 +1,26 @@
 package com.yash.yotaapi.domain;
 
-import java.util.List;
+import java.util.Date;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -54,8 +65,49 @@ public class Test {
 	 */
 	private String description;
 	
+	/** Start date of the test. */
+    @NotNull(message = "Start date cannot be Null")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private Date startDate;
+ 
+    /** End date of the test. */
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @NotNull(message = "End date cannot be null")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private Date endDate;
 	
-	//@OneToMany(cascade = CascadeType.ALL)
-    //private List<Question> questions;
+    /** Date when the test was created. */
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @CreatedDate
+    private Date createdAt;
+ 
+    /** Date when the test was last updated. */
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @LastModifiedDate
+    private Date updatedAt;
+    
+    /** Set of questions associated with the test. */
+    @OneToMany(mappedBy = "test",cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Question> questions;
+    
+    /** Set of associates details associated with the test. */
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "test_id")
+    private Set<AssociateDetails> associates;
+    
+    /** Pre-persist action to set the created date. */
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = new Date();
+    }
+ 
+    /** Pre-update action to set the updated date. */
+    @PreUpdate
+    public void onUpdate() {
+        this.updatedAt = new Date();
+    }
 
 }
